9299e91414262919440b525cbdea1f1456e5f356
Jkasnese <guiga994@gmail.com>
2018-06-27 11:01:28 -0300

    TODO: Implement ArrayPlayer

--- src/environment/ArrayPlayer.java
+++ src/environment/ArrayPlayer.java
@@ -4,6 +4,12 @@ package environment;
 	Player that has all possible board configurations and the play to it.
 ***/
 public class ArrayPlayer extends Player {
+
+	private char[][] plays;
+
+	public ArrayPlayer (){
+		this.plays = new char[564][9];
+	}
 	
 	// Picks a random winning position and tries to win with it no matter what.
 	private int goal;
@@ -22,7 +28,10 @@ public class ArrayPlayer extends Player {
 
 
 	/***
-		
+		This function gets the next play.
+		In a given board configuration, first find what is the "correct" representation in the hashmap.
+		Pass the hash to the hashmap and obtain the array index.
+		Use the given index to find the play in the player chromossome
 	***/
 	@Override
 	public void getNextPlay(char[] board) {
