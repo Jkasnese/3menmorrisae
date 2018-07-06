75333aeda04c01caf75c1e9c4aa6e0021ee68a0d
Aluno <aluno@MP53-004576.uefs.br>
2018-07-06 11:08:52 -0300

    Added opponents serialization

--- src/environment/Running.java
+++ src/environment/Running.java
@@ -28,7 +28,7 @@ public class Running {
 
 	public static void main(String[] args) {
 
-
+        // OBJECTS
         // Generate the board/position representations
 		Representation representation = new Representation();
 
@@ -39,30 +39,73 @@ public class Running {
         System.out.println("Generating benchmark players");
         ArrayList<Player> opponents = new ArrayList<Player>();
         
+        // Uncomment for random opponents:
         for (int i=0; i<Running.OPPONENTS_AMOUNT; i++){
             opponents.add(new ArrayPlayer(representation));
         }
-        
+
+        // Uncomment for evolved opponents:
+        FileInputStream opponentsIn = new FileInputStream("opponents.ser");
+        ObjectInputStream in = new ObjectInputStream(opponentsIn);
+
+        while(!EOF){
+            try {
+                e = (ArrayPlayer) in.readObject();
+                opponents.add(e);    
+            } catch{
+                
+            }
+            
+        }
+
+
+
         // Generate first generation        
         System.out.println("Generating first generation!");
         for (int i=0; i<Running.POPULATION_SIZE; i++){
             players.add(new ArrayPlayer(representation));
         }
-        
-        // BEGINING OF GENERATIONS
+
+        // If this is a run to generate an opponent pool, uncomment the following lines between /* -- */:
+        // File to output oponents.
+        FileOutputStream opponentsFile = new FileOutputStream("opponents.ser");
+        ObjectOutputStream out = new ObjectOutputStream(opponentsFile);
+
+
+
+
+        // - - - - - BEGINING OF GENERATIONS - - - - -
+
+
         for (int i=0; i<Running.NUMBER_OF_GENERATIONS; i++) {
         	
         	// EVALUATION METRICS
     		int MAX_FITNESS = 0;
     		double AVG_FITNESS = 0;
+            int maxWins = 0;
+            int numWinsBestPlayer = 0;
+            int numDrawsBestPlayer = 0;
+            int numLossesBestPlayer = 0;
+            int avgWins = 0;
+            int avgDraws = 0;
+            int avgLosses = 0;
 
         	// Each player plays in a tourney to determine fitness
         	for (int j=0; j<Running.POPULATION_SIZE; j++) {
         		Player tourneyPlayer = players.get(j);
+
+                // Zero previous status
         		tourneyPlayer.setFitness(0); // Reset fitness from previous tourneys
+                tourneyPlayer.wins = 0;
+                tourneyPlayer.draws = 0;
+                tourneyPlayer.losses = 0;
         		
         		// Select opponents and play games to evaluate fitness;
         		int newFitness = 0;
+                int newWins = 0;
+                int newDraws = 0;
+                int newLosses = 0;
+
         		char[] emptyboard = new char[9];
         		Arrays.fill(emptyboard, '0');
         		for (int k=0; k<Running.OPPONENTS_AMOUNT; k++) {
@@ -74,43 +117,80 @@ public class Running {
                     //System.out.println("Ganhador do jogo entre " + tourneyPlayer.getId() + " e " + opponent.getId() + " foi: " + winner);
         			if (winner == tourneyPlayer.getId()) {
         				newFitness += Running.WIN_POINTS;
+                        newWins++;
         			} else if (winner == -1) {
         				newFitness += Running.DRAW_POINTS;
+                        newDraws++;
         			} else {
         				newFitness += Running.LOSE_POINTS;
+                        newLosses++;
         			}
         			game = new Game(opponents.get(k), tourneyPlayer, representation);
         			winner = game.play();
         			//System.out.println("Ganhador do jogo entre " + opponent.getId() + " e " + tourneyPlayer.getId() + " foi: " + winner);
         			if (winner == tourneyPlayer.getId()) {
         				newFitness += Running.WIN_POINTS;
+                        newWins++;
         			} else if (winner == -1) {
         				newFitness += Running.DRAW_POINTS;
+                        newDraws++;
         			} else {
         				newFitness += Running.LOSE_POINTS;
+                        newLosses++;
         			}
         		}
         		
-        		//System.out.println("Player " + tourneyPlayer.getId() + " fitness " + newFitness);
-        		// Give newFitness
+                // Give newFitness and status
         		tourneyPlayer.setFitness(newFitness);
+                tourneyPlayer.wins = newWins;
+                tourneyPlayer.wins = newDraws;
+                tourneyPlayer.wins = newLosses;
         		
         		// EVALUATION METRICS
+
+                    // Best player metrics
         		if (newFitness > MAX_FITNESS) {
         			MAX_FITNESS = newFitness;
+                    numWinsBestPlayer = newWins;
+                    numDrawsBestPlayer = newDraws;
+                    numLossesBestPlayer = newLosses;
         		}
+
+                    // Max wins
+                if (newWins > maxWins){
+                    maxWins = newWins;
+                }
+
+                    // Avg status
         		AVG_FITNESS += newFitness;
+                avgWins += newWins;
+                avgDraws += newDraws;
+                avgLosses += newLosses;
         	}
 
         	AVG_FITNESS /= Running.POPULATION_SIZE;
+            avgWins /= Running.POPULATION_SIZE;
+            avgDraws /= Running.POPULATION_SIZE;
+            avgLosses /= Running.POPULATION_SIZE;
         	
     		// After everyone played their games, sort based on fitness and breed the best.
     		players.sort(null);
+
+            // If this is a run to generate an opponent pool, uncomment the following lines between /* -- */:
+            // ops determine the number of opponents of a generation to be saved
+            for (int ops = 0; ops < 5; ops++){
+                try{
+                    out.writeObject(players.get(ops));
+                } catch (IOException i){
+                    i.printStackTrace();
+                }
+            }
+
     		
     		graph_generator.add_value((double) i, MAX_FITNESS, 0);
     		graph_generator.add_value((double)i, AVG_FITNESS, 1);
     		
-    		// Breed
+    		// Select %ALLOWED_BREED parents to breed and fill the remainder of the population
     		int parent = 0;
     		int allowedToBreed = (int) (Running.POPULATION_SIZE * Running.ALLOWED_BREED);
     		for (int b = allowedToBreed; b < Running.POPULATION_SIZE; b++) {
@@ -126,6 +206,10 @@ public class Running {
     		aefaef++;
         } // Generations loop end
         
+        // If this is a run to generate an opponent pool, uncomment the following lines between /* -- */:
+        out.close();
+        opponentsFile.close();
+
         graph_generator.generate_graph();
         graph_generator.pack();
         graph_generator.setVisible(true);
